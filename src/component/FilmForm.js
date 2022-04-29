706496816a478d422ae1f9b435f57ff9b15a2d8c
Ma <s300570@studenti.polito.it>
2022-04-29 14:18:05 +0200

    finished edit,add,delete and fixed bugs

--- src/component/FilmForm.js
+++ src/component/FilmForm.js
@@ -11,6 +11,47 @@ function FilmForm(props){
     // const [date,setDate] = useState(dayjs());
     const [date,setDate] = useState(props.film.date? props.film.date.format('YYYY-MM-DD'):undefined);
     const [rating,setRating] = useState(props.film.rating? props.film.rating:0);
+    const [stars,setStars] = useState(()=>{
+        let stararr= [];
+        if(props.film.rating ===undefined){
+            for(let i=0;i<5;++i){
+                stararr.push(<Button key={i} id={i} onClick={()=>{clickHandler(i)}}><BsStar /></Button>);
+            }
+        }else{
+            for(let i=0;i<props.film.rating;++i){
+                stararr.push(<Button key={i+'f'} id={i} onClick={()=>{clickHandler(i)}}><BsStarFill/></Button>);
+            }
+            for(let i=props.film.rating;i<5;++i){
+                stararr.push(<Button key={i+'b'} id={i} onClick={()=>{clickHandler(i)}}><BsStar/></Button>);
+            }
+        }
+    
+        return stararr;
+    });
+    const clickHandler=(id)=>{
+        setRating(id+1);
+        setStars((stars)=>{
+            // let stararr= [];
+            return stars.map((s,idx)=>{
+                if(idx<=id)
+                    return <Button key={idx+'f'} id={idx} onClick={()=>{clickHandler(idx)}}><BsStarFill/></Button>
+                else
+                    return <Button key={idx+'b'} id={idx} onClick={()=>{clickHandler(idx)}}><BsStar/></Button> 
+            })
+            // return stararr;
+        })
+        // stararr = stararr.map((s,idx)=>  (idx<=id)?
+        //     <Button key={idx+'f'} id={idx} onClick={()=>{clickHandler(idx)}}><BsStarFill/></Button>:
+        //     <Button key={idx+'b'} id={idx} onClick={()=>{clickHandler(idx)}}><BsStar/></Button> 
+        
+        // );
+
+        // for(let i of stararr){
+        //     console.log(i.props.children.type);
+        // }
+        
+        
+    }
     const handleSubmit=(event)=>{
         event.preventDefault();
         let film={}
@@ -23,7 +64,6 @@ function FilmForm(props){
         if(props.film === undefined)
             props.addFilm(film);
         else{
-            console.log(film);
             props.editFilm(film);
         }
     }
@@ -50,8 +90,8 @@ function FilmForm(props){
                     {/* <Form.Control type="number" min={0} max={5} value={rating} onChange={event => setRating(event.target.value)}/> */}
                     {/* <Form.Control  min={0} max={5} value={createStars()} onChange={event => setRating(event.target.value)}/> */}
                     {/* <BsStar></BsStar>????????????????????????????????????????????????????? */}
-                    <ShowStars film={props.film} setRating={setRating}/>
-                    
+                    {/* <ShowStars film={props.film} setRating={setRating}/> */}
+                    {stars}
                 </Form.Group>
                 <Form.Group className='mb-3' as={Col}>
                     <Col sm={3}>
@@ -66,34 +106,5 @@ function FilmForm(props){
         </Form>
     )
 }
-function ShowStars(props){
-    let stars= [];
-    const clickHandler=(id)=>{
-        props.setRating(id+1);
-        stars.map((s,idx)=>  (idx<=id)?
-            <Button key={idx+'f'} id={idx} onClick={()=>{clickHandler(idx)}}><BsStarFill/></Button>:
-            <Button key={idx+'b'} id={idx} onClick={()=>{clickHandler(idx)}}><BsStar/></Button> 
-        
-        );
-        for(let i of stars){
-            console.log(i.props.children.type);
-        }
-        
-        
-    }
-    if(props.film.rating ===undefined){
-        for(let i=0;i<5;++i){
-            stars.push(<Button key={i} id={i} onClick={()=>{clickHandler(i)}}><BsStar /></Button>);
-        }
-    }else{
-        for(let i=0;i<props.film.rating;++i){
-            stars.push(<Button key={i+'f'} id={i} onClick={()=>{clickHandler(i)}}><BsStarFill/></Button>);
-        }
-        for(let i=props.film.rating;i<5;++i){
-            stars.push(<Button key={i+'b'} id={i} onClick={()=>{clickHandler(i)}}><BsStar/></Button>);
-        }
-    }
 
-    return stars;
-}
 export{FilmForm}
